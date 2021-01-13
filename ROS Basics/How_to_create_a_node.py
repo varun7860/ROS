3a693330c34cc0dcca1198f1d70a00675fbc38fb
Varun walimbe <51876661+varun7860@users.noreply.github.com>
2021-01-13 13:45:21 +0530

    Update How_to_create_a_node.py

--- ROS Basics/How_to_create_a_node.py
+++ ROS Basics/How_to_create_a_node.py
@@ -3,12 +3,12 @@ import rospy
 
 def main():
     '''init_node() takes argument which is the name of the node
-       you can put any name of node you want. You can make node
+       you can put any name of node you want. You can make the
        node anonymous by putting anonymous = True as second
-       argument'''
+       argument, however this is optional.'''
     
     #create a new node in ros by rospy.init_node() function.
-    rospy.init_node('new_node')
+    rospy.init_node('new_node', anonymous = True)
     
     #print on the terminal by rospy.loginfo() function
     rospy.loginfo('Node created successfully')
